f1cf8fd8cb61d34cacee12b0fdb47121d0129597
Romeo Rizzi <romeo.rizzi@univr.it>
2020-04-23 08:46:03 +0200

    aggiunti link per problema poldo

---
+++ classes/Algoritmi/path/2020-04-21/README.md
@@ -0,0 +1,5 @@
+Se vui sapere di più su come produrre ( ed in tempo O(n log n) ) non solo una longest increasing subsequence ma anche una colorazione col minimo numeri di colori (che si certificano mutualmente in ottimalità) ecco alcune valide sorgenti:
+
+https://en.wikipedia.org/wiki/Patience_sorting
+https://www.cs.princeton.edu/courses/archive/spring13/cos423/lectures/LongestIncreasingSubsequence.pdf
+http://wordaligned.org/articles/patience-sort
--- classes/Algoritmi/path/2020-04-21/poldo.py
+++ classes/Algoritmi/path/2020-04-21/poldo.py
@@ -59,3 +59,10 @@ print(opt)
 
 assert opt == num_max_panini(1,0), "cross validation v1 versus v2"
 print(f"OK: testcase passed. N={N}, opt={opt}")
+
+""" Se vui sapere di più su come produrre ( ed in tempo O(n log n) ) non solo una longest increasing subsequence ma anche una colorazione col minimo numeri di colori (che si certificano mutualmente in ottimalità) ecco alcune valide sorgenti:
+
+https://en.wikipedia.org/wiki/Patience_sorting
+https://www.cs.princeton.edu/courses/archive/spring13/cos423/lectures/LongestIncreasingSubsequence.pdf
+http://wordaligned.org/articles/patience-sort
+"""
